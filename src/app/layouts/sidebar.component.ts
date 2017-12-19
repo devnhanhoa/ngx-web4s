3e30f0362f66974480542bae49c8a7604565bcf6
cuongnh <hungcuongqn86@gmail.com>
2017-12-19 09:59:45 +0700

    Start

--- src/app/layouts/sidebar.component.ts
+++ src/app/layouts/sidebar.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AppService} from '../service/app.service';
+import {rootUri} from '../app.config';
 
 @Component({
     selector: 'app-layouts-sidebar',
@@ -7,14 +8,19 @@ import {AppService} from '../service/app.service';
     styleUrls: ['./sidebar.component.css']
 })
 
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
     private subs: any;
     public menuRoot = [];
     public allMenu = [];
     public select = {Module: {id: ''}};
+    private uri: string;
 
     constructor(public appService: AppService) {
+        this.uri = rootUri + document.location.pathname;
         this.getSidebar();
+    }
+
+    ngOnInit() {
     }
 
     private getSidebar() {
@@ -23,12 +29,27 @@ export class SidebarComponent {
             data => {
                 this.menuRoot = data.data.menu_root;
                 this.allMenu = data.data.all_menu;
+                this.setActive();
                 this.appService.http.endLoad();
             },
             error => {
                 this.appService.http.endLoad();
             }
         );
+    }
+
+    public setActive() {
+        let group = '';
+        Object.keys(this.allMenu).map((index) => {
+            if ((this.allMenu[index].Module.slug.trim() !== '') && this.uri.indexOf(this.allMenu[index].Module.slug.trim()) > -1) {
+                group = this.allMenu[index].Module.group;
+            }
+        });
+        Object.keys(this.menuRoot).map((index) => {
+            if (this.menuRoot[index].Module.group === group) {
+                this.selectItem(this.menuRoot[index]);
+            }
+        });
     }
 
     public selectItem(item) {
