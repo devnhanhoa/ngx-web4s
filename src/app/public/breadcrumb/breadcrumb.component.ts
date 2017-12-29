26da06ba09112f4ff73d890c084de0c69c643560
cuongnh <hungcuongqn86@gmail.com>
2017-12-29 09:07:18 +0700

    ignore proxy

--- src/app/public/breadcrumb/breadcrumb.component.ts
+++ src/app/public/breadcrumb/breadcrumb.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit, Input} from '@angular/core';
-import {Router, ActivatedRoute, NavigationEnd, Params, PRIMARY_OUTLET} from '@angular/router';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/observable/of';
 
