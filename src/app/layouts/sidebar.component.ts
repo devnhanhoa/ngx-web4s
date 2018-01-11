bf6be2bf614355feb21a223fbf75acb752d2298b
ngannk <ngannkbk@gmail.com>
2018-01-11 08:51:42 +0700

    #13

--- src/app/layouts/sidebar.component.ts
+++ src/app/layouts/sidebar.component.ts
@@ -64,7 +64,7 @@ export class SidebarComponent implements OnInit {
     }
 
     public selectItem(item) {
-        this.select = item;
+        this.select = (this.select === item ? null : item);
     }
 
     public minMenu() {
