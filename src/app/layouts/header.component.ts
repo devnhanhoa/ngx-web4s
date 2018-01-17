2a7888f54f503e1b9b7556930f09451c5f6f9bb6
ngannk <ngannkbk@gmail.com>
2018-01-17 11:50:33 +0700

    #18

--- src/app/layouts/header.component.ts
+++ src/app/layouts/header.component.ts
@@ -12,16 +12,25 @@ export class HeaderComponent {
 
     private subs: any;
     public profile = {full_name: '', url_img: ''};
+    public syssetting = {title: ''};
     public rootUrl = rootUrl;
 
     constructor(public appService: AppService) {
         this.getProfile();
+        this.getSysSetting();
     }
 
     private getProfile() {
         this.subs = this.appService.getProfile().subscribe(
             data => {
                 this.profile = data.data;
+            }
+        );
+    }
+    private getSysSetting() {
+        this.subs = this.appService.getSysSetting().subscribe(
+            data => {
+                this.syssetting = data.data;
             }
         );
     }
