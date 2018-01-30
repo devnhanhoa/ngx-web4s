f9eccc012dc97f61f8d701fbcdf749349cbe8d45
cuongnh <hungcuongqn86@gmail.com>
2018-01-30 10:13:36 +0700

    #12

--- src/app/public/notification/notification.component.ts
+++ src/app/public/notification/notification.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AppService} from '../../service/app.service';
 
 @Component({
-  selector: 'app-public-notification',
-  templateUrl: './notification.component.html',
-  styleUrls: ['./notification.component.css']
+    selector: 'app-public-notification',
+    templateUrl: './notification.component.html',
+    styleUrls: ['./notification.component.css']
 })
 export class NotificationComponent implements OnInit {
 
-  constructor(public appService: AppService) {
-  }
+    constructor(public appService: AppService) {
+    }
 
-  ngOnInit() {
-  }
-  reload() {
-    window.location.reload();
-  }
+    ngOnInit() {
+    }
 
+    public closeAlert(item) {
+        const index = this.appService.http.alertsys.indexOf(item);
+        this.appService.http.alertsys.splice(index, 1);
+    }
 }
